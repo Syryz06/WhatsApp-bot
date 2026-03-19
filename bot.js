f7c3a63973c9d70eeb28b8298fdec58ad8c4945b
Syryz06 <as1900169@gmail.com>
2026-03-19 18:25:47 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,15 @@ const YOUR_NUMBER = process.env.YOUR_NUMBER;
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
-        executablePath: '/run/current-system/sw/bin/chromium',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
-});
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu'
+    ]
+}
+
 
 const conversations = {};
 
